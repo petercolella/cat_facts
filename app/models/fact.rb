dab87fab5d24b7466b71e42987cb70d712649eda
petercolella <peter.joseph.colella@gmail.com>
2021-03-12 22:01:17 -0500

    refactors fact.rb with new catfact url

--- app/models/fact.rb
+++ app/models/fact.rb
@@ -1,10 +1,10 @@
 class Fact < ApplicationRecord
 	def self.retrieve(num)
-		url = "http://catfacts-api.appspot.com/api/facts?number=#{num}"
+		url = "https://catfact.ninja/facts?limit=#{num}"
 		response = HTTParty.get(url)
-		parsed = JSON.parse(response)
-		cat_facts_test = parsed["facts"]
+		parsed = JSON.parse(response.body)
+		cat_facts_test = parsed["data"]
 		# binding.pry
-		cat_facts = JSON.parse(response)["facts"]
+		cat_facts = JSON.parse(response.body)["data"]
 	end
 end
